f2c51c358ad6f26394a5d70ff171f153f4cf4924
AnthonyLimanto <s3898395@student.rmit.edu.au>
2022-09-03 16:02:33 +1000

    progress on task3

--- Task3.cpp
+++ Task3.cpp
@@ -4,12 +4,28 @@
 #include <vector>
 #include <fstream>
 #include <bits/stdc++.h>
+#include <sys/stat.h>
 
 #define THIRD_CHAR 2
 
 void map3();
 bool compareIndex(int lhs, int rhs);
 void* sortByThirdChar(void* arg) ;
+std::vector<int> index1;
+std::vector<int> index2;
+std::vector<int> index3;
+std::vector<int> index4;
+std::vector<int> index5;
+std::vector<int> index6;
+std::vector<int> index7;
+std::vector<int> index8;
+std::vector<int> index9;
+std::vector<int> index10;
+std::vector<int> index11;
+std::vector<int> index12;
+std::vector<int> index13;
+int aaa = 3;
+std::vector<int> tests = {1,2,3,4,5,6,7,12,12,13,14,15};
 
 std::vector<std::string> global;
 
@@ -33,45 +49,187 @@ int main (int argc, char* argv[]) {
 bool compareIndex(int lhs, int rhs) {
     return global[lhs][THIRD_CHAR] < global[rhs][THIRD_CHAR];
 }
+
 void* sortByThirdChar(void* arg) {
       // lambda function for custom compare in sort()
     auto compare = [&](const int& lhs, const int& rhs) -> bool {
     return global[lhs][THIRD_CHAR] < global[rhs][THIRD_CHAR];
     };
-
-    std::vector<int>* vec = static_cast<std::vector<int>*>(arg);
+    
+    
+    std::vector<int>* vec = (std::vector<int>*)(arg);
     std::sort(vec->begin(), vec->end(), compare);
-
-    if (vec[0].size() == 4) {
+    
+//     for (auto i : *vec) {
+//         std::cout << global[i] << std::endl;
+// }
+    if (global[(*vec)[0]].size() == 4) {
         std::ofstream myFile("Task3_wordlist/length3.txt");
-        for (int i : *vec) {
-            myFile << global[i];
-            myFile.close();
+        for (auto i : *vec) {
+        myFile << global[i] << std::endl;
         }
+        myFile.close();
     }
-    return nullptr;
- }
-
-void map3() {
+    // if (global[(*vec)[0]].size() == 5) {
+    //     std::ofstream myFile("Task3_wordlist/length4.txt");
+    //     for (auto i : *vec) {
+    //     myFile << global[i] << std::endl;
+    //     }
+    //     myFile.close();
+    // }
+}
+// void* sort2(void* arg) {
 //       // lambda function for custom compare in sort()
 //     auto compare = [&](const int& lhs, const int& rhs) -> bool {
 //     return global[lhs][THIRD_CHAR] < global[rhs][THIRD_CHAR];
-// };
+//     };
+    
+//     std::sort(index2.begin(), index2.end(), compare);
+    
+//     for (auto i : index2) {
+//         std::cout << global[i] << std::endl;
+//     }
+//  }
+//  void* sort3(void* arg) {
+//       // lambda function for custom compare in sort()
+//     auto compare = [&](const int& lhs, const int& rhs) -> bool {
+//     return global[lhs][THIRD_CHAR] < global[rhs][THIRD_CHAR];
+//     };
+    
+//     std::sort(index3.begin(), index3.end(), compare);
+    
+//     for (auto i : index3) {
+//         std::cout << global[i] << std::endl;
+//     }
+//  }
+//  void* sort4(void* arg) {
+//       // lambda function for custom compare in sort()
+//     auto compare = [&](const int& lhs, const int& rhs) -> bool {
+//     return global[lhs][THIRD_CHAR] < global[rhs][THIRD_CHAR];
+//     };
+    
+//     std::sort(index4.begin(), index4.end(), compare);
+    
+//     for (auto i : index4) {
+//         std::cout << global[i] << std::endl;
+//     }
+//  }
+//  void* sort5(void* arg) {
+//       // lambda function for custom compare in sort()
+//     auto compare = [&](const int& lhs, const int& rhs) -> bool {
+//     return global[lhs][THIRD_CHAR] < global[rhs][THIRD_CHAR];
+//     };
+    
+//     std::sort(index5.begin(), index5.end(), compare);
+    
+//     for (auto i : index5) {
+//         std::cout << global[i] << std::endl;
+//     }
+//  }
+//  void* sort6(void* arg) {
+//       // lambda function for custom compare in sort()
+//     auto compare = [&](const int& lhs, const int& rhs) -> bool {
+//     return global[lhs][THIRD_CHAR] < global[rhs][THIRD_CHAR];
+//     };
+    
+//     std::sort(index6.begin(), index6.end(), compare);
+    
+//     for (auto i : index6) {
+//         std::cout << global[i] << std::endl;
+//     }
+//  }
+//  void* sort7(void* arg) {
+//       // lambda function for custom compare in sort()
+//     auto compare = [&](const int& lhs, const int& rhs) -> bool {
+//     return global[lhs][THIRD_CHAR] < global[rhs][THIRD_CHAR];
+//     };
+    
+//     std::sort(index7.begin(), index7.end(), compare);
+    
+//     for (auto i : index7) {
+//         std::cout << global[i] << std::endl;
+//     }
+//  }
+//  void* sort8(void* arg) {
+//       // lambda function for custom compare in sort()
+//     auto compare = [&](const int& lhs, const int& rhs) -> bool {
+//     return global[lhs][THIRD_CHAR] < global[rhs][THIRD_CHAR];
+//     };
+    
+//     std::sort(index8.begin(), index8.end(), compare);
+    
+//     for (auto i : index8) {
+//         std::cout << global[i] << std::endl;
+//     }
+//  }
+//  void* sort9(void* arg) {
+//       // lambda function for custom compare in sort()
+//     auto compare = [&](const int& lhs, const int& rhs) -> bool {
+//     return global[lhs][THIRD_CHAR] < global[rhs][THIRD_CHAR];
+//     };
+    
+//     std::sort(index9.begin(), index9.end(), compare);
+    
+//     for (auto i : index9) {
+//         std::cout << global[i] << std::endl;
+//     }
+//  }
+//  void* sort10(void* arg) {
+//       // lambda function for custom compare in sort()
+//     auto compare = [&](const int& lhs, const int& rhs) -> bool {
+//     return global[lhs][THIRD_CHAR] < global[rhs][THIRD_CHAR];
+//     };
+    
+//     std::sort(index10.begin(), index10.end(), compare);
+    
+//     for (auto i : index10) {
+//         std::cout << global[i] << std::endl;
+//     }
+//  }
+//  void* sort11(void* arg) {
+//       // lambda function for custom compare in sort()
+//     auto compare = [&](const int& lhs, const int& rhs) -> bool {
+//     return global[lhs][THIRD_CHAR] < global[rhs][THIRD_CHAR];
+//     };
+    
+//     std::sort(index11.begin(), index11.end(), compare);
+    
+//     for (auto i : index11) {
+//         std::cout << global[i] << std::endl;
+//     }
+//  }
+//  void* sort12(void* arg) {
+//       // lambda function for custom compare in sort()
+//     auto compare = [&](const int& lhs, const int& rhs) -> bool {
+//     return global[lhs][THIRD_CHAR] < global[rhs][THIRD_CHAR];
+//     };
+    
+//     std::sort(index12.begin(), index12.end(), compare);
+    
+//     for (auto i : index12) {
+//         std::cout << global[i] << std::endl;
+//     }
+//  }
+//  void* sort13(void* arg) {
+//       // lambda function for custom compare in sort()
+//     auto compare = [&](const int& lhs, const int& rhs) -> bool {
+//     return global[lhs][THIRD_CHAR] < global[rhs][THIRD_CHAR];
+//     };
+    
+//     std::sort(index13.begin(), index13.end(), compare);
+    
+//     for (auto i : index13) {
+//         std::cout << global[i] << std::endl;
+//     }
+//  }
+
+void map3() {
+      // lambda function for custom compare in sort()
+    auto compare = [&](const int& lhs, const int& rhs) -> bool {
+    return global[lhs][THIRD_CHAR] < global[rhs][THIRD_CHAR];
+};
    
     // index vectors
-    std::vector<int> index1;
-    std::vector<int> index2;
-    std::vector<int> index3;
-    std::vector<int> index4;
-    std::vector<int> index5;
-    std::vector<int> index6;
-    std::vector<int> index7;
-    std::vector<int> index8;
-    std::vector<int> index9;
-    std::vector<int> index10;
-    std::vector<int> index11;
-    std::vector<int> index12;
-    std::vector<int> index13;
 
     pthread_t t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13;
     
@@ -119,19 +277,20 @@ void map3() {
         }
     }
     // TO DO make each of these done by a seperate thread
-    pthread_create(&t1, NULL, sortByThirdChar, &index1);
-    pthread_create(&t2, NULL, sortByThirdChar, &index2);
-    pthread_create(&t3, NULL, sortByThirdChar, &index3);
-    pthread_create(&t4, NULL, sortByThirdChar, &index4);
-    pthread_create(&t5, NULL, sortByThirdChar, &index5);
-    pthread_create(&t6, NULL, sortByThirdChar, &index6);
-    pthread_create(&t7, NULL, sortByThirdChar, &index7);
-    pthread_create(&t8, NULL, sortByThirdChar, &index8);
-    pthread_create(&t9, NULL, sortByThirdChar, &index9);
-    pthread_create(&t10, NULL, sortByThirdChar, &index10);
-    pthread_create(&t11, NULL, sortByThirdChar, &index11);
-    pthread_create(&t12, NULL, sortByThirdChar, &index12);
-    pthread_create(&t13, NULL, sortByThirdChar, &index13);
+    pthread_create(&t1, NULL, &sortByThirdChar, &index1);
+    pthread_create(&t2, NULL, &sortByThirdChar, &index2);
+    // pthread_create(&t3, NULL, &sortByThirdChar, &index3);
+    // pthread_create(&t4, NULL, &sortByThirdChar, &index4);
+    // pthread_create(&t5, NULL, &sortByThirdChar, &index5);
+    // pthread_create(&t6, NULL, &sortByThirdChar, &index6);
+    // pthread_create(&t7, NULL, &sortByThirdChar, &index7);
+    // pthread_create(&t8, NULL, &sortByThirdChar, &index8);
+    // pthread_create(&t9, NULL, &sortByThirdChar, &index9);
+    // pthread_create(&t10, NULL, &sortByThirdChar, &index10);
+    // pthread_create(&t11, NULL, &sortByThirdChar, &index11);
+    // pthread_create(&t12, NULL, &sortByThirdChar, &index12);
+    // pthread_create(&t13, NULL, &sortByThirdChar, &index13);
+
 
     // pthread_create(&t2, NULL, std::sort(index2.begin(), index2.end(), compare), NULL);
     // pthread_create(&t3, NULL, std::sort(index3.begin(), index3.end(), compare), NULL);
@@ -145,14 +304,15 @@ void map3() {
     // pthread_create(&t11, NULL, &std::sort(index11.begin(), index11.end(), compare), NULL);
     // pthread_create(&t12, NULL, &std::sort(index12.begin(), index12.end(), compare), NULL);
     // pthread_create(&t13, NULL, &std::sort(index13.begin(), index13.end(), compare), NULL);
-
-    // Testing index1
-    for (std::vector<std::string>::size_type i = 0; i != index1.size(); i++) {
-        std::cout << global[index1[i]] << std::endl;
-    }
-
-
-
+    pthread_join(t1, NULL);
     
+    // Testing index1
+    // for (std::vector<std::string>::size_type i = 0; i != index1.size(); i++) {
+    //     std::cout << global[index1[i]] << std::endl;
+    // }
+    
+    // for (int i : index1) {
+    //     std::cout << global[index1[i]] << std::endl;
+    // }
 
 }
